7cc49088d8d3b7a86191276aeba3929c44400aeb
JJulia9 <juliaworkjj@gmail.com>
2024-01-12 12:10:56 +0000

    home page basics

---
+++ partials/Footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <a href="tel:+4474357575757"><img src="../assets/images/phone.png" alt="">+44743 5757 5757</a> 
+    <a href="mailto:bigGatsby@gmail.com"><img src="../assets/images/email.png" alt="">bigGatsby@gmail.com</a> 
+    <a href="https://www.instagram.com/big_Gatsby"><img src="../assets/images/instagram.png" alt="">big_Gatsby</a> 
+
+</footer>
+
+</body>
+</html>
