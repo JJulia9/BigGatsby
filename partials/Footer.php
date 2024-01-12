f65a8d490e236a0254be9bb92f862e700bae763a
JJulia9 <juliaworkjj@gmail.com>
2024-01-12 12:23:19 +0000

    main page

--- partials/Footer.php
+++ partials/Footer.php
@@ -1,9 +1,16 @@
 <footer>
-    <a href="tel:+4474357575757"><img src="../assets/images/phone.png" alt="">+44743 5757 5757</a> 
-    <a href="mailto:bigGatsby@gmail.com"><img src="../assets/images/email.png" alt="">bigGatsby@gmail.com</a> 
-    <a href="https://www.instagram.com/big_Gatsby"><img src="../assets/images/instagram.png" alt="">big_Gatsby</a> 
+    <a href="tel:+4474357575757"><img src="assets/images/phone.png" alt="">+44743 5757 5757</a> 
+    <a href="mailto:bigGatsby@gmail.com"><img src="assets/images/email.png" alt="">bigGatsby@gmail.com</a> 
+    <a href="https://www.instagram.com/big_Gatsby"><img src="assets/images/instagram.png" alt="">big_Gatsby</a> 
 
 </footer>
 
+
+<script src="https://tympanus.net/codrops/adpacks/cda_sponsor.js"></script>
+		<script src="js/gsap.min.js"></script>
+		<script src="js/ScrollTrigger.min.js"></script>
+		<script src="js/lenis.min.js"></script>
+		<script src="js/imagesloaded.pkgd.min.js"></script>
+		<script type="module" src="../assets/js/script.js"></script>
 </body>
 </html>
