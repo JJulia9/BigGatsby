c8ef30a8e480ababb5f2b160687cf39293c9288d
JJulia9 <juliaworkjj@gmail.com>
2024-01-16 22:05:42 +0000

    adding grid

--- index.php
+++ index.php
@@ -9,20 +9,78 @@ Ethan Anderson
 Ava Richardson
 Liam Harris
 Sophia Miller
-Jackson Carter
+Carter 
 Isabella Turner
 Aiden Campbell
 Emma Clark
-Lucas Mitchell</p>
+ </p>
 
 <h1 class="hero-h1">BIg Gatsby</h1>
 <h3 class="hero-h3">theatre</h3>
 
-<div class="hero-string">
- 03.03 Broadway Extravaganza: A Night of Musical Delights  13.03 Shakespearean Showcase: Classic Tales Reimagined   18.03 Emerging Playwrights Festival: Unveiling New Voices
-</div>
+
+
+<div class="lineBox">
+        <p class="box__line">03.03 Broadway Extravaganza: A Night of Musical Delights  13.03 Shakespearean Showcase: Classic Tales Reimagined   18.03 Emerging Playwrights Festival: Unveiling New Voices
+		</p>
+    </div>
 
 </section>
+
+
+<div class="lines">
+	<img src="assets/images/lines.svg" alt="">
+</div>
+
+
+<!-- <nav class="menu">
+				<div class="menu__item menu__item--1" data-direction="bt">
+					<div class="menu__item-inner">
+						<div class="mainmenu">
+							<a href="#" class="mainmenu__item">Story</a>
+							<a href="#" class="mainmenu__item">Chronicles</a>
+							<a href="#" class="mainmenu__item">Tour</a>
+							<a href="#" class="mainmenu__item">Contact</a>
+						</div>
+						<p class="label label--topleft label--vert-mirror">the important stuff</p>
+						<p class="label label--bottomright label--vert">made in bannockburn</p>
+					</div>
+				</div>
+				<div class="menu__item menu__item--2" data-direction="lr">
+					<div class="menu__item-inner">
+						<div class="menu__item-map"></div>
+						<a href="#" class="menu__item-hoverlink">The location</a>
+					</div>
+				</div>
+				<div class="menu__item menu__item--3" data-direction="bt">
+					<div class="menu__item-inner">
+						<div class="sidemenu">
+							<a href="#" class="sidemenu__item"><span class="sidemenu__item-inner">The Gameplay</span></a>
+							<a href="#" class="sidemenu__item"><span class="sidemenu__item-inner">About LARP</span></a>
+							<a href="#" class="sidemenu__item"><span class="sidemenu__item-inner">The Rules</span></a>
+							<a href="#" class="sidemenu__item"><span class="sidemenu__item-inner">History</span></a>
+							<a href="#" class="sidemenu__item"><span class="sidemenu__item-inner">People</span></a>
+							<a href="#" class="sidemenu__item"><span class="sidemenu__item-inner">Join</span></a>
+							<a href="#" class="sidemenu__item"><span class="sidemenu__item-inner">...</span></a>
+						</div>
+					</div>
+				</div>
+				<div class="menu__item menu__item--4" data-direction="rl">
+					<div class="menu__item-inner">
+						<p class="label label--topleft label--line">Join us now</p>
+						<a href="#" class="menu__item-link">Learn how to <br> participate</a>
+					</div>
+				</div>
+				<div class="menu__item menu__item--5" data-direction="tb">
+					<div class="menu__item-inner">
+						<p class="quote">Hail to thee, our infantry, still brave, beyond the grave</p>
+					</div>
+				</div>
+				<button class="action action--menu"><svg class="icon icon--menu"><use xlink:href="#icon-menu"></use></svg></button>
+				<button class="action action--close"><svg class="icon icon--close"><use xlink:href="#icon-close"></use></svg></button>
+			</nav> -->
+
+
 
 <div class="slider">
 	<div class="slider-wrapper">
@@ -71,7 +129,73 @@ Lucas Mitchell</p>
 </div>
 
 
+<h2 class="main-header">What's on</h2>
 
+
+<svg xmlns="http://www.w3.org/2000/svg" width="1321" height="1022" viewBox="0 0 1321 1022" fill="none">
+  <path d="M1445.4 318.016C1515.13 365.682 1564.31 444.48 1548.2 527.393C1493.5 808.834 1420.11 1044.36 848.154 1020.25C180.817 992.117 1076.43 376.308 268.55 373.045C-539.326 369.782 695.977 -80.2829 1136.79 13.4155C1517.03 94.2374 1253.53 186.856 1445.4 318.016Z" fill="#960B17"/>
+</svg>
+	
+<div class="home-grid">
+
+	<div class="grid-item1">
+
+		<img src="assets/images/event1.jpg" alt="event" srcset="" >
+	</div>
+
+	<div class="grid-item2">
+		<img src="assets/images/event2.jpg" alt="event" srcset="">
+	</div>
+
+	<div class="grid-item3">
+		<img src="assets/images/event3.jpg" alt="event" srcset=""  >
+	</div>
+
+	<div class="grid-item4">
+		<p>february events</p>
+	</div>
+
+	<div class="grid-item5">
+		<img src="assets/images/event4.jpg" alt="event" srcset=""  >
+	</div>
+
+</div>
+
+<section class="blog">
+	<h2 class="section-header">
+	ticles ar
+    </h2>
+
+	<div class="blog-wrapper">
+		<div class="blog-img">
+			<img src="assets/images/blog1.png" alt="" srcset="">
+		</div>
+
+		<div class="blog-articles">
+			
+			<hr>
+			<div class="article">
+			<p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+			</div>
+
+			<hr>
+			<div class="article">
+			<p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+			</div>
+
+			<hr>
+			<div class="article">
+			<p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+			</div>
+
+			<hr>
+			<div class="article">
+			<p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+			</div>
+
+		</div>
+	</div>
+	</section>
     
 
 
